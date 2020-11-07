2ca6ee612a3ae6f900016fb8cd3fde464637ce41
alen <grafuxa@gmail.com>
2020-11-07 13:31:27 +0100

    added ids to inputs

--- script/app.js
+++ script/app.js
@@ -2,3 +2,11 @@ import { formDomObj } from "./form";
 const formDom = document.querySelector("#form");
 
 formDom.innerHTML = formDomObj;
+
+
+["phoneVal","fullNameVal","emailVal","msgVal"];
+
+
+const msgVal = document.querySelector("#msgVal");
+
+console.log(msgVal.value);
--- script/form.js
+++ script/form.js
@@ -1,37 +1,36 @@
 export const formDomObj = `
 <div class="field">
-  <label class="label">Name</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="Text input">
+  <label class="label">Full Name</label>
+  <div class="control  has-icons-left">
+    <input id="fullNameVal" class="input" type="text" placeholder="Text input" value="John Doe">
+    <span class="icon is-small is-left">
+    <i class="fas fa-user"></i>
+  </span>
   </div>
 </div>
 
+
 <div class="field">
-  <label class="label">Username</label>
+  <label class="label">Phone number</label>
   <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+    <input id="phoneVal" class="input" type="text" placeholder="Text input" value="666-666-666">
     <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
+      <i class="fas fa-mobile-alt"></i>
     </span>
   </div>
-  <p class="help is-success">This username is available</p>
+
 </div>
+
 
 <div class="field">
   <label class="label">Email</label>
   <div class="control has-icons-left has-icons-right">
-    <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+    <input id="emailVal" class="input" type="email" placeholder="Email input" value="hello@">
     <span class="icon is-small is-left">
       <i class="fas fa-envelope"></i>
     </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-exclamation-triangle"></i>
-    </span>
   </div>
-  <p class="help is-danger">This email is invalid</p>
+ 
 </div>
 
 <div class="field">
@@ -49,37 +48,15 @@ export const formDomObj = `
 <div class="field">
   <label class="label">Message</label>
   <div class="control">
-    <textarea class="textarea" placeholder="Textarea"></textarea>
+    <textarea id="msgVal" class="textarea" placeholder="Textarea">This is message for tests</textarea>
   </div>
 </div>
 
-<div class="field">
-  <div class="control">
-    <label class="checkbox">
-      <input type="checkbox">
-      I agree to the <a href="#">terms and conditions</a>
-    </label>
-  </div>
-</div>
 
-<div class="field">
-  <div class="control">
-    <label class="radio">
-      <input type="radio" name="question">
-      Yes
-    </label>
-    <label class="radio">
-      <input type="radio" name="question">
-      No
-    </label>
-  </div>
-</div>
 
 <div class="field is-grouped">
   <div class="control">
     <button class="button is-link">Submit</button>
   </div>
-  <div class="control">
-    <button class="button is-link is-light">Cancel</button>
-  </div>
+
 </div>`;
